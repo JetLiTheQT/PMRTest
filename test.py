7968637afaa59c1b180f3e801edee0b185156394
Ekkachai Jet Ittihrit <ekkachai.ittihrit2@gmail.com>
2023-05-07 18:02:40 -0700

    Update test.py removing credentials and sdk file location

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from firebase_admin import messaging
 from datetime import datetime
 
 # Change source for the adminsdk, youll find it in app/src/ within the github repo
-cred = credentials.Certificate("C:\\Users\\ekkac\\AndroidStudioProjects\\PMRTest\\app\\src\\pmr-test-cd8d2-firebase-adminsdk-fo4jd-05259879c0.json") 
+cred = credentials.Certificate("[DIRECTORY LOCATION FOR YOUR FIRE-ADMINSDK JSON]") 
 firebase_admin.initialize_app(cred)
 
 def send_push_notification(token, message):
@@ -25,10 +25,8 @@ def send_push_notification(token, message):
     response = messaging.send(message)
     print(f'Successfully sent message: {response}')
 
-#Jet's Emulator
-device_token = "dEopJtYDS1apTu3wAQ_BOY:APA91bFkR7rvI5VVELN-g3HuVVzeTI5DQq8Z8Axwm70CxUWwjY6m338J6VYOtqTLDug86JqwiDGoV5gH3CyV707B3G90Kubv7QLyJXTRANNUatAOutUyIYbbAi9_SgYE31F1jpczTH53"
-#Jet's Phone
-# device_token = "dVIDmjV4Qqi9xTTxDfGSVp:APA91bGrh33TtKXRV_k6otUeuKk1ew-3LdayQJ_6y4ZsLv_geZNMNGXsC_Jd9mHk9NPdv9LMuWvsYIwz83aaeXZYILr_CdQl5Dh-NG8fVe99iU0NB8VsEQpVTvevAjAKhGSJKE9AIRLt"
+device_token = "[FCM TOKEN]"
+#FCM Token is retrievable through the app after un-commenting out the Retrieve_token() function in the LogScreen page. You should find it output in Android Studio's Log. Input the device token above.
 message = "Patient has fallen"
 #Tested, only works on LAN to update UI..
 
